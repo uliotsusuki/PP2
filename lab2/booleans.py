23ce3d30a43dedbb58eee2a6ecacfb999154b86f
uliotsusuki <109410789+uliotsusuki@users.noreply.github.com>
2025-02-22 14:53:17 +0500

    Add files via upload

---
+++ lab2/booleans.py
@@ -0,0 +1,24 @@
+#ex1
+print(5>3)
+#True
+
+#ex2
+print(10 > 9)
+#True
+
+#ex3
+print(10==9)
+#false
+
+#ex4
+print(10 < 9)
+#False
+
+#ex5
+print(bool("abc"))
+#True
+
+#ex6
+print(bool(0))
+#False
+
